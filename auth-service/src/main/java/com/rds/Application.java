a7e9af9cab7c6e69439f508564cadc8b9a64b262
rishal <devrishal@gmail.com>
2022-06-24 17:20:48 +0200

    Added flyway scripts for creating tables

--- auth-service/src/main/java/com/rds/Application.java
+++ auth-service/src/main/java/com/rds/Application.java
@@ -1,5 +1,6 @@
 package com.rds;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
@@ -7,5 +8,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties
 public class Application {
     public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
     }
 }
